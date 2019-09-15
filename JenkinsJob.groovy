8e2de92fa960abc102d93d68d5fa6c69d452b7d2
Rishab Shah <Rishab@Shah>
2019-09-16 00:44:20 +0530

    Info on groovy added..

--- JenkinsJob.groovy
+++ JenkinsJob.groovy
@@ -3,6 +3,6 @@ import jenkins.model.Jenkins;
 // Get Jenkins instance
 def j = Jenkins.getInstance();
 // Get the job we wan't to trigger
-def job = j.getItem("B");
+def job = j.getItem("reference-job");
 // Finally we schedule a new build which starts directly (the zero in the argument)
 job.scheduleBuild2(0)
