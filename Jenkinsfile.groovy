4ae9ad478d6d988cbe5479cc579e1b043eeb29e7
Rishab Shah <Rishab@Shah>
2019-09-15 06:23:35 +0530

    Made few changes

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -13,19 +13,22 @@ def jobParameters = [
   name:          'MyJob_2',
   description:   'Build of my STG environment : https://stg.mycompany.com',
   repository:    'https://github.com/riXab/groovy-pipeline-scripting.git',
-  branch:        'master' //,
- // credentialId:  ''
+  branch:        'master' ,
+  credentialId:  ''
 ]
 
 // define repo configuration
 def branchConfig                =   [new BranchSpec(jobParameters.branch)]
-def userConfig                  =   [new UserRemoteConfig(jobParameters.repository, null, null, /*jobParameters.credentialId*/null)]
+def userConfig                  =   [new UserRemoteConfig(jobParameters.repository, null, null, jobParameters.credentialId)]
+//def userConfig                  =   [new UserRemoteConfig(jobParameters.repository, null, null, /*jobParameters.credentialId*/null)]
 //def cleanBeforeCheckOutConfig   =   new CleanBeforeCheckout()
 //def sparseCheckoutPathConfig    =   new SparseCheckoutPaths([new SparseCheckoutPath("Jenkinsfile")])
 //def cloneConfig                 =   new CloneOption(true, true, null, 3)
 //def extensionsConfig            =   [cleanBeforeCheckOutConfig,sparseCheckoutPathConfig,cloneConfig]
-def scm                         =   new GitSCM(userConfig, branchConfig, false, [], null, null, /*extensionsConfig*/null)
+def scm                         =   new GitSCM(userConfig, branchConfig, false, [], null, null, null)
+//def scm                         =   new GitSCM(userConfig, branchConfig, false, [], null, null, /*extensionsConfig*/null)
 
+echo "Define flow"
 // define SCM flow
 def flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "Jenkinsfile")
 
@@ -35,15 +38,20 @@ flowDefinition.setLightweight(true)
 // get Jenkins instance
 Jenkins jenkins = Jenkins.getInstance()
 
+echo "Create Job"
 // create the job
 def job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(jenkins,jobParameters.name)
 
+echo "Define Job"
 // define job type
 job.definition = flowDefinition
 
 // set job description
 job.setDescription(jobParameters.description)
 
+echo "Save Job"
 // save to disk
 jenkins.save()
+
+echo "Reload Job"
 jenkins.reload()
