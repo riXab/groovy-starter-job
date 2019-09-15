2c7bdb9616565b682d7307ea4599683feff48160
Rishab Shah <Rishab@Shah>
2019-09-16 04:35:07 +0530

    Info on groovy added..

--- jenkinsFree.groovy
+++ jenkinsFree.groovy
@@ -9,6 +9,18 @@ import hudson.triggers.*
 import hudson.model.FreeStyleProject
 //import hudson.triggers.TimerTrigger
 
+
+node('master'){
+	
+	
+def jdk = tool name: 'localJDK'
+env.JAVA_HOME = "${jdk}"
+
+withEnv(["PATH+MAVEN=${tool 'localMaven'}/bin"]) {
+				bat 'mvn -B verify'
+			}
+
+}
 // parameters
 echo "Start Groovy.."
 echo "Set Parameter for Job - Test Job"
@@ -25,13 +37,6 @@ hudson.triggers.TimerTrigger newCron = new hudson.triggers.TimerTrigger(spec);
 newCron.start(job, true);
 job.addTrigger(newCron);
 //job.buildTrigger("my-groove")
-
-def jdk = tool name: 'localJDK'
-env.JAVA_HOME = "${jdk}"
-
-withEnv(["PATH+MAVEN=${tool 'localMaven'}/bin"]) {
-				bat 'mvn -B verify'
-			}
  archive '**/*.war'
 
 
