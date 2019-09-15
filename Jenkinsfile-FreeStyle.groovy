1f6e536ed4242dd6e49cb9f8b362f08da1c27d52
Rishab Shah <Rishab@Shah>
2019-09-16 01:08:27 +0530

    Info on groovy added..

--- Jenkinsfile-FreeStyle.groovy
+++ Jenkinsfile-FreeStyle.groovy
@@ -4,9 +4,19 @@ import hudson.plugins.git.*
 import hudson.plugins.git.extensions.*
 import hudson.plugins.git.extensions.impl.*
 import jenkins.model.Jenkins
+import hudson.triggers.TimerTrigger
+import hudson.model.*
+import jenkins.model.*
+import hudson.slaves.*
+import javaposse.jobdsl.plugin.*
+import java.util.Collections
+import java.util.List
+import hudson.plugins.git.extensions.GitSCMExtension
+
+
 
 @NonCPS
-   def createFreestyleProject(){
+ def createFreestyleProject(){
 	   String repository = "https://github.com/riXab/groovy-pipeline-scripting.git";
        def parent = Jenkins.getInstance()
        //Instantiate a new project
@@ -24,14 +34,38 @@ import jenkins.model.Jenkins
         //Adding and saving the job property to the project
        project.addProperty(jobProperty)
 	 println "Property Set"
-		//project.getSCM("https://github.com/riXab/groovy-pipeline-scripting.git")
-		def branchConfig = [new BranchSpec("master")]
-		def userConfig = [new UserRemoteConfig(repository, null, null, null)]
-		project.setSCM(new hudson.plugins.git.GitSCM(repository))
+	   
+	   //set build trigger cron to run daily
+	 project.addTrigger(new TimerTrigger("* * * * *"))
+
+//set post build steps
+def publishersList = project.getPublishersList()
+publishersList.add(new hudson.tasks.BuildTrigger("my-groove", true))
+
+//set SCM
+List<BranchSpec> branches = Collections.singletonList(new BranchSpec("*/master"));
+List<SubmoduleConfig> submoduleCnf = Collections.<SubmoduleConfig>emptyList();
+// We are using predefined user id jenkins. You change it in the global config
+List<UserRemoteConfig> usersconfig = Collections.singletonList(
+        new UserRemoteConfig(repository, '', '', '')
+)
+List<GitSCMExtension> gitScmExt = new ArrayList<GitSCMExtension>();
+def scm = new GitSCM(usersconfig, branches, false, submoduleCnf, null, null, gitScmExt)
+project.setScm(scm)
+
+project.save()
+
+Jenkins.instance.restart()
+
+
+	 //project.getSCM("https://github.com/riXab/groovy-pipeline-scripting.git")
+//		def branchConfig = [new BranchSpec("master")]
+//		def userConfig = [new UserRemoteConfig(repository, null, null, null)]
+//		project.setSCM(new hudson.plugins.git.GitSCM(repository))
 		//project.setSCM(new hudson.plugins.git.GitSCM(userConfig, branchConfig, false, [], null, null, null))
-	println "SCM Set"	
-       project.save()
-       parent.reload()
+//	println "SCM Set"	
+  //     project.save()
+    //   parent.reload()
 
 }
 
