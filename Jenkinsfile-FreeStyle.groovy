94c2baaf0feeabf2b044d4c554b1988db14adece
Rishab Shah <Rishab@Shah>
2019-09-15 06:12:29 +0530

    Added another groocy

---
+++ Jenkinsfile-FreeStyle.groovy
@@ -0,0 +1,24 @@
+import com.cloudbees.groovy.cps.NonCPS
+import hudson.model.FreeStyleProject
+
+@NonCPS
+   def createFreestyleProject(String projectName){
+       def parent = Jenkins.getInstance()
+       //Instantiate a new project
+       def project = new FreeStyleProject(parent, projectName);
+        //Set a description for the project
+       project.setDescription("Just a placeholder for a description")
+        //Create a parameter for the project
+       def parameterDefinitions = new ArrayList<ParameterDefinition>();
+       def name = "ParameterOne"
+       def defaultValue = "1"
+       def description = "Just a placeholder for a parameter description"
+       parameterDefinitions.add(new StringParameterDefinition(name, defaultValue, description))
+        //Create a job property for the project
+       def jobProperty = new ParametersDefinitionProperty(parameterDefinitions)
+        //Adding and saving the job property to the project
+       project.addProperty(jobProperty)
+       project.save()
+       parent.reload()
+
+}
