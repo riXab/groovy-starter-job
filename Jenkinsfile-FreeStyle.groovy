eec26de2b6fbdac9891a44fa8a82036cb94f51ce
Rishab Shah <Rishab@Shah>
2019-09-16 08:28:42 +0530

    Info on groovy added...

--- Jenkinsfile-FreeStyle.groovy
+++ Jenkinsfile-FreeStyle.groovy
@@ -82,9 +82,9 @@ println "Trying something out.."
 
 
 def buildersList = project.getBuildersList()
-buildersList.add(new hudson.tasks.Maven("clean package", "localMaven", false, new jenkins.mvn.DefaultSettingsProvider(), new jenkins.mvn.DefaultGlobalSettingsProvider(), false))
-
-
+//buildersList.add(new hudson.tasks.Maven("clean package", "localMaven", false, new jenkins.mvn.DefaultSettingsProvider(), new jenkins.mvn.DefaultGlobalSettingsProvider(), false))
+buildersList.add(new hudson.tasks.Maven("clean package", "localMaven")
+	
 //set post build steps
 def publishersList = project.getPublishersList()
 publishersList.add(new hudson.tasks.BuildTrigger("my-groove", true))
