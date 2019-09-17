04803e6406f18f4da5ee516aa2b393cc22a62da9
Rishab Shah <Rishab@Shah>
2019-09-17 09:42:05 +0530

    Commited post build

--- Jenkinsfile-FreeStyle.groovy
+++ Jenkinsfile-FreeStyle.groovy
@@ -92,8 +92,8 @@ buildersList.add(new hudson.tasks.Maven("clean package", "localMaven"))
 //set post build steps
 def publishersList = project.getPublishersList()
 //publishersList.add(new hudson.tasks.BuildTrigger("my-groove, MyJob_3", false))
-publishersList.add(new hudson.tasks.BuildTrigger("my-style-check", false))
-//publishersList.add(new hudson.tasks.BuildTrigger("asf", false))
+publishersList.add(new hudson.tasks.BuildTrigger("my-style-check", true))
+publishersList.add(new hudson.tasks.BuildTrigger("asf", true))
 publishersList.add(new hudson.tasks.ArtifactArchiver("**/*.war", "", false, false))
 
 project.save()
