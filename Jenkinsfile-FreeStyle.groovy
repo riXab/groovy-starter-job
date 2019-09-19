3cc0808d54f7e7ac6b11abb54bdf065123374720
Rishab Shah <Rishab@Shah>
2019-09-19 10:48:14 +0530

    ..

--- Jenkinsfile-FreeStyle.groovy
+++ Jenkinsfile-FreeStyle.groovy
@@ -98,7 +98,7 @@ publishersList.add(new hudson.tasks.BuildTrigger("asf", false))
 // Get Jenkins instance
 def j = Jenkins.getInstance();
 // Get the job we wan't to trigger
-def job = j.getItem("my-style-check");
+def job = j.getItem("asf");
 // Finally we schedule a new build which starts directly (the zero in the argument)
 job.scheduleBuild2(0)
 
