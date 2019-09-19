2c807fb0b8d5573fd285733bb2257c0c364c3b0c
Rishab Shah <Rishab@Shah>
2019-09-19 10:54:14 +0530

    ..

--- Jenkinsfile-FreeStyle.groovy
+++ Jenkinsfile-FreeStyle.groovy
@@ -26,15 +26,15 @@ env.JAVA_HOME = "${jdk}"
         //Set a description for the project
        project.setDescription("Just a placeholder for a description")
         //Create a parameter for the project
-       def parameterDefinitions = new ArrayList<ParameterDefinition>();
-       def name = "ParameterOne"
-       def defaultValue = "1"
-       def description = "Just a placeholder for a parameter description"
-       parameterDefinitions.add(new StringParameterDefinition(name, defaultValue, description))
+//       def parameterDefinitions = new ArrayList<ParameterDefinition>();
+//       def name = "ParameterOne"
+//       def defaultValue = "1"
+//       def description = "Just a placeholder for a parameter description"
+//       parameterDefinitions.add(new StringParameterDefinition(name, defaultValue, description))
         //Create a job property for the project
-       def jobProperty = new ParametersDefinitionProperty(parameterDefinitions)
+//       def jobProperty = new ParametersDefinitionProperty(parameterDefinitions)
         //Adding and saving the job property to the project
-       project.addProperty(jobProperty)
+//       project.addProperty(jobProperty)
 	 println "Property Set"
 	   
 	   //set build trigger cron to run daily
